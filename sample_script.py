c2af3da2b12b16ca9ada03d2dba1aa21e3fc40c1
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 12:53:48 +0200

    implementation...

--- sample_script.py
+++ sample_script.py
@@ -1,6 +1,6 @@
 import os
 
-a5_open_project('/usr/archivio/2032_AI/Rotor37/vN2/rotor_g_v4.trb')
+a5_open_project('/usr/archivio/2032_AI/Rotor37/vN2/rotor_g_v4/rotor_g_v4_Database/_mdb/_flow_000022/_mesh/rotor_g_v4.trb')
 #a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
 a5_import_and_replace_geometry_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
 a5_generate_3d()
