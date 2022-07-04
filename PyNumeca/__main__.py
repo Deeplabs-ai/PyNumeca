82a8d95b72eaca7579231e2dc70babd092c4dc8d
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 15:30:02 +0200

    Trying batch version

--- PyNumeca/__main__.py
+++ PyNumeca/__main__.py
@@ -6,7 +6,8 @@ import os
 def main():
     script_path = os.path.join(os.getcwd(), 'sample_script.py')
     generate_mesh_script(filename=script_path,
-                         default_trb_path='/usr/archivio/2032_AI/Rotor37/vN2/rotor_g_v4/rotor_g_v4_Database/_mdb/_flow_000022/_mesh/rotor_g_v4.trb',
+                         default_trb_path='/usr/archivio/2032_AI/Rotor37/vN2/rotor_g_v4/rotor_g_v4_Database/_mdb'
+                                          '/_flow_000022/_mesh/rotor_g_v4.trb',
                          geomturbo_path='/home/marco/PyNumeca/rotor_g_v4.geomTurbo',
                          export_path='/home/marco/PyNumeca/a5_project_sample.trb',)
-    run_igg_script(ui=True, path=script_path)
+    run_igg_script(ui=False, path=script_path)
