ec5994d68a6d73e9987620d11963a916bf22da8c
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 12:51:54 +0200

    implementation...

--- sample_script.py
+++ sample_script.py
@@ -1,8 +1,6 @@
 import os
 
-from PyNumeca.igg.igg import initialize_from_geom_turbo
-
-# initialize_from_geom_turbo(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
-a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
-
+a5_open_project('/usr/archivio/2032_AI/Rotor37/vN2/rotor_g_v4.trb')
+#a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
+a5_import_and_replace_geometry_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
 a5_generate_3d()
