791925c04271c11c0a1c1618a3b82dc493e69582
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 16:08:14 +0200

    bug fix

--- PyNumeca/fine/utils.py
+++ PyNumeca/fine/utils.py
@@ -7,7 +7,7 @@ def run_fine_script(path: str, ui: bool = False):
     if not os.path.exists(path):
         raise FileNotFoundError('Cannot find {0}'.format(path))
 
-    command = 'fine{0}'.format(constants.version)
+    command = 'fine{0} '.format(constants.version)
     if not ui:
         command += '-batch '
 
