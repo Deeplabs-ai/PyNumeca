66ec35fe54de788eb84659979babf8c424891084
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 12:19:04 +0200

    Updating... (II)

--- sample_script.py
+++ sample_script.py
@@ -1,4 +1,4 @@
 import os
 from PyNumeca.igg.igg import load_geom_turbo
 
-load_geom_turbo(os.path.join(os.getcwd(), 'sample_script.py'))
+load_geom_turbo(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
