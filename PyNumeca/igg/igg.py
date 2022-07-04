013c0677c09949cbf11aaf4f159d1f94c54a742b
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 12:46:38 +0200

    adding new functions to sample_script.py (II)

--- PyNumeca/igg/igg.py
+++ PyNumeca/igg/igg.py
@@ -1,2 +1,2 @@
-def load_geom_turbo(path):
+def initialize_from_geom_turbo(path):
     a5_import_geometry_file(path)
--- sample_script.py
+++ sample_script.py
@@ -1,4 +1,8 @@
 import os
 
-a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
+from PyNumeca.igg.igg import initialize_from_geom_turbo
+
+initialize_from_geom_turbo(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
+# a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
+
 a5_generate_3d()
