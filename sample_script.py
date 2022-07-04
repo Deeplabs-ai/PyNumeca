f567ae6e2abf0f9f1f5645705be907dfaae71253
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-04 12:41:54 +0200

    bug fixing (II)

--- sample_script.py
+++ sample_script.py
@@ -1,3 +1,3 @@
-# import os
+import os
 
-a5_init_new_project_from_a_geomTurbo_file('/home/marco/PyNumeca/rotor_g_v4.geomTurbo')
+a5_init_new_project_from_a_geomTurbo_file(os.path.join(os.getcwd(), 'rotor_g_v4.geomTurbo'))
