0591399b77cb56345cc67270907e478ba56829a5
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-07 13:57:30 +0200

    removing copy of .iec file

--- PyNumeca/igg/igg.py
+++ PyNumeca/igg/igg.py
@@ -6,15 +6,15 @@ from PyNumeca.constants import constants
 
 def a5_mesh_from_geomturbo(source_trb_path: str,
                            geomturbo_path: str,
-                           output_trb_path: str,
+                           # output_trb_path: str,
                            output_igg_path: str,
                            igg_version: str = constants.version,
                            cores: int = 1,
                            ):
 
-    shutil.copy(source_trb_path, output_trb_path)
+    # shutil.copy(source_trb_path, output_trb_path)
     cmd = f"igg{igg_version}" + " -print -real-batch -autogrid5" + " -numproc " + str(
-        cores) + " -trb " + '"' + output_trb_path + '"' +\
+        cores) + " -trb " + '"' + source_trb_path + '"' +\
           " -geomTurbo " + '"' + geomturbo_path + '"' + " -mesh " +\
           '"' + output_igg_path + '"'
 
