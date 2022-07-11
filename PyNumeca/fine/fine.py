dd071416dca92597beea790e4186e9e39dc447d4
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-07-11 10:37:03 +0200

    Now we can run the computation in batch mode or not

--- PyNumeca/fine/fine.py
+++ PyNumeca/fine/fine.py
@@ -11,10 +11,9 @@ def fine_run_from_mesh(source_iec_path: str,
                        fine_version: str = constants.version,
                        index: int = 1,
                        ):
-
     shutil.copy(source_iec_path, output_iec_path)
-    cmd = f"fine{fine_version}" + " -print -batch" + " -project " + '"' +\
-          output_iec_path + '"' + " -mesh " + '"' + igg_path + '"' +\
+    cmd = f"fine{fine_version}" + " -print -batch" + " -project " + '"' + \
+          output_iec_path + '"' + " -mesh " + '"' + igg_path + '"' + \
           " -index " + str(index)
 
     print("Running command '" + cmd + "'")
@@ -29,23 +28,27 @@ def setup_parallel_computation(
         memory_real: int = constants.memory_real,
         memory_int: int = constants.memory_int,
 ):
-    cmd = f"fine{fine_version}" + " -print -batch -partition" + " -computation " +\
+    cmd = f"fine{fine_version}" + " -print -batch -partition" + " -computation " + \
           '"' + run_file_path + '"' + " -nproc " + str(
-           cores) + " -load_balancing " + str(balancing_fine) + " -nbint " + str(memory_int) + " -nbreal " + str(
-           memory_real)
+        cores) + " -load_balancing " + str(balancing_fine) + " -nbint " + str(memory_int) + " -nbreal " + str(
+        memory_real)
 
     print("Running command '" + cmd + "'")
     os.system(cmd)
 
 
-def run_parallel_computation(run_file_path: str):
+def run_parallel_computation(run_file_path: str, batch: bool = False):
     run_file_path = os.path.split(run_file_path)[0]
     run_file_base_name = os.path.splitext(os.path.split(run_file_path)[1])[0]
     batch_file = os.path.join(run_file_path, run_file_base_name + '.batch')
-    # cmd = '"' + batch_file + '"'
-    cmd =  batch_file
 
-    print("Running command '" + cmd + "'")
-    process = subprocess.Popen(cmd)
-    return process.pid
-    # os.system(cmd)
+    if batch:
+        cmd = batch_file
+        print("Running command '" + cmd + "'")
+        process = subprocess.Popen(cmd)
+        return process.pid
+    else:
+        cmd = '"' + batch_file + '"'
+        print("Running command '" + cmd + "'")
+        os.system(cmd)
+        return
