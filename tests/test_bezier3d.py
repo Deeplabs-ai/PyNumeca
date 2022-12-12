201ee10ee4a53556d6b359d8d6f6cc9540dcd4d0
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-12-12 11:07:46 +0100

    bug fix

---
+++ tests/test_bezier3d.py
@@ -0,0 +1,19 @@
+from PyNumeca.reader.numecaParser import numecaParser
+import numpy as np
+import matplotlib.pyplot as plt
+from PyNumeca.preprocessing.bezier3d import get_bezier_parameters, bezier_curve
+import plotly.graph_objects as go
+from PyNumeca.preprocessing.bezier_geomturbo import BezierBlade, BezierCompressor
+
+
+def test_bezier3d_compressor():
+    samples = ['data/C2_rev4.geomTurbo', "data/C2_Mappe_DB_51_r2_final.geomTurbo"]
+    for sample in samples:
+        bc = BezierCompressor(sample,
+                              [[7, 6, 6, 7], [7, 6, 6, 7], [6, 7, 6, 6, 7, 6]],
+                              [True, True, False],
+                              [[100, 20, 20, 100],
+                              [100, 20, 20, 100],
+                              [20, 100, 20, 20, 100, 20]])
+        bc.fit_compressor_with_bezier()
+        bc.get_compressor_from_control_points()
