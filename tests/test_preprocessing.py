45299e25013d88afd6df5926312039b31a9dd54b
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2023-01-27 14:22:02 +0100

    Updatings tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -10,7 +10,7 @@ def test_centrifugal_compressor():
 
     compressor = turbo.CentrifugalCompressor(fluid=FluidsList.Air)
     compressor.load_geometry_from_geometurbo(geomturbo)
-    compressor.load_boundaries_from_mf(mf)
+    compressor.load_boundaries_and_performances_from_mf(mf)
 
     compressor.save('tmp.bin')
     new_compressor = turbo.CentrifugalCompressor.load('tmp.bin')
